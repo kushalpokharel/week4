c3bbd5e28dd7c0480814065eed63271f7264bdd3
kushalpokharel <kusaalpokharel@gmail.com>
2022-05-31 04:28:47 +0545

    assignment completed

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,15 +1,78 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
-import Head from "next/head"
-import React from "react"
+import { providers, Contract, utils } from "ethers"
+import Head from "next/head";
+import React, { useEffect } from "react"
 import styles from "../styles/Home.module.css"
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json";
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    style: {
+        color: "white"
+    }
+});
+
+const schema = yup.object({
+    name: yup
+        .string()
+        .required()
+        .min(3)
+        .max(25),
+
+    address: yup
+        .string()
+        .required(),
+
+    age: yup
+        .number()
+        .required()
+        .positive()
+        .integer()
+        .min(10)
+        .max(90)
+});
 
 export default function Home() {
-    const [logs, setLogs] = React.useState("Connect your wallet and greet!")
 
-    async function greet() {
+    const classes = useStyles();
+    const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+    const [greetings, setGreetings] = React.useState("");
+    useEffect(() => {
+        const contract = new Contract(
+            '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+            Greeter.abi,
+        )
+
+        const provider = new providers.JsonRpcProvider('http://localhost:8545')
+
+        const contractOwner = contract.connect(provider.getSigner())
+
+        contractOwner.on('NewGreeting', (greeting) => {
+            const message = utils.parseBytes32String(greeting);
+            console.log(message);
+            setGreetings(greetings);
+        });
+    }, []);
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            address: '',
+            age: 20,
+        },
+        validationSchema: schema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    async function greet(msg: string) {
         setLogs("Creating your Semaphore identity...")
 
         const provider = (await detectEthereumProvider()) as any
@@ -28,7 +91,7 @@ export default function Home() {
 
         setLogs("Creating your Semaphore proof...")
 
-        const greeting = "Hello world"
+        const greeting = msg;
 
         const witness = Semaphore.genWitness(
             identity.getTrapdoor(),
@@ -74,8 +137,60 @@ export default function Home() {
 
                 <div className={styles.logs}>{logs}</div>
 
-                <div onClick={() => greet()} className={styles.button}>
-                    Greet
+                <div>
+                    <form onSubmit={formik.handleSubmit}>
+
+                        <TextField
+                            inputProps={{ className: classes.style }}
+                            InputLabelProps={{ className: classes.style }}
+                            margin="normal"
+                            fullWidth
+                            id="name"
+                            name="name"
+                            label="Name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            error={formik.touched.name && Boolean(formik.errors.name)}
+                            helperText={formik.touched.name && formik.errors.name}
+
+                        />
+                        <TextField
+                            inputProps={{ className: classes.style }}
+                            InputLabelProps={{ className: classes.style }}
+                            margin="normal"
+                            fullWidth
+                            id="address"
+                            name="address"
+                            label="Address"
+                            value={formik.values.address}
+                            onChange={formik.handleChange}
+                            error={formik.touched.address && Boolean(formik.errors.address)}
+                            helperText={formik.touched.address && formik.errors.address}
+
+                        />
+                        <TextField
+                            inputProps={{ className: classes.style }}
+                            InputLabelProps={{ className: classes.style }}
+                            margin="normal"
+                            fullWidth
+                            id="age"
+                            name="age"
+                            label="Age"
+                            type="number"
+                            value={formik.values.age}
+                            onChange={formik.handleChange}
+                            error={formik.touched.age && Boolean(formik.errors.age)}
+                            helperText={formik.touched.age && formik.errors.age}
+
+                        />
+                        <Button color="primary" variant="contained" fullWidth type="submit">
+                            Submit
+                        </Button>
+
+                        <div onClick={() => greet(formik.values.address)} className={styles.button}>
+                            Greet
+                        </div>
+                    </form>
                 </div>
             </main>
         </div>
